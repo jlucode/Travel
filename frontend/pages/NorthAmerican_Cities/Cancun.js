e0df6e1dd1e6776359bab49944536f2eea092fc1
jlucode <78547511+jlucode@users.noreply.github.com>
2022-12-16 12:20:11 -0600

    Update Cancun.js

--- frontend/pages/NorthAmerican_Cities/Cancun.js
+++ frontend/pages/NorthAmerican_Cities/Cancun.js
@@ -12,6 +12,11 @@ import HomeIcon from '@mui/icons-material/Home';
 
 export default function Cancun() {
 
+axios.defaults.headers.common['Access-Control-Allow-Origin'] = '*';
+axios.defaults.headers.common['Access-Control-Allow-Methods'] = 'GET, POST, OPTIONS';
+axios.defaults.headers.common['Access-Control-Allow-Headers'] = '*';
+axios.defaults.headers.common['Access-Control-Expose-Headers'] = 'Content-Length, Content-Range';
+
 const [status, setStatus] = useState("idle");
 const [ipaddressdata , setIpaddressData] = useState([{
     data: "",
@@ -41,13 +46,7 @@ useEffect(() => {
 
          /* Conditional IF Statement included for. Flight origin and destination logically be the same. If so, API is not fetched. */
          if(response.data.iata !== destination){
-        const response2 = await axios.get(`https://api.travelpayouts.com/v2/prices/latest?currency=usd&period_type=year&page=1&duration=hour&limit=5&origin=${response.data.iata}&destination=${destination}&show_to_affiliates=true&sorting=price&token=1fb2829993009e6a7db3163d4d00a1ff`, {headers: {
-            'Access-Control-Allow-Origin': '*',
-            'Access-Control-Allow-Methods': 'GET, POST, OPTIONS',
-            'Access-Control-Allow-Headers': 'DNT,User-Agent, X-Requested-With, If-Modified-Since, Cache-Control, Content-Type, Range',
-            'Access-Control-Expose-Headers': 'Content-Length, Content-Range',
-
-        },})
+        const response2 = await axios.get(`https://api.travelpayouts.com/v2/prices/latest?currency=usd&period_type=year&page=1&duration=hour&limit=5&origin=${response.data.iata}&destination=${destination}&show_to_affiliates=true&sorting=price&token=1fb2829993009e6a7db3163d4d00a1ff`)
         setFlightData(response2.data)
         setStatus2("resolved")
         }
